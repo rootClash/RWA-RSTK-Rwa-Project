ea418ece7569627e7ece0eb8cfef4c0eb4e23996
hackflu <satyamkumar45381@gmail.com>
2026-04-01 09:18:22 +0000

    feat : add the script and test

---
+++ script/DeploySRSTK.s.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+import {Script} from "forge-std/Script.sol";
+import {SRSTK} from "../src/RSTK/SRSTK.sol";
+import {TokenScript} from "../script/Helper/TokenScript.s.sol";
+import {HelperScript} from "../script/Helper/HelperScript.s.sol";
+import {IContractStruct} from "../src/RSTK/IContractStruct.sol";
+
+contract DeploySRSTK is Script {
+    SRSTK public s_srstk;
+    HelperScript public s_scriptPriceOracle;
+    TokenScript public s_tokenScript;
+    function run(string memory source,address srstkTokenAddr,address priceOracleAddr,address portfolioContractAddr) public {
+        s_scriptPriceOracle = new HelperScript(source);
+        (IContractStruct.RequestData memory config  , )= s_scriptPriceOracle.getNetworkConfig();
+        s_tokenScript = new TokenScript();
+        IContractStruct.ExtraInfo memory extraInfo = s_tokenScript.getNetworkConfig();
+        extraInfo.srstkTokenAddr = srstkTokenAddr;
+        extraInfo.priceOracleAddr = priceOracleAddr;
+        extraInfo.portfolioContractAddr = portfolioContractAddr;
+        vm.startBroadcast();
+        s_srstk = new SRSTK(
+            config,
+            extraInfo
+        );
+        vm.stopBroadcast();
+    }
+}
--- test/unit/SRSTKTest.t.sol
+++ test/unit/SRSTKTest.t.sol
@@ -2,24 +2,50 @@
 pragma solidity ^0.8.19;
 import {Test} from "forge-std/Test.sol";
 import {SRSTK} from "../../src/RSTK/SRSTK.sol";
-import {TokenScript} from "../../script/Helper/TokenScript.s.sol";
-import {SRSTKConstant} from "../../src/RSTK/SRSTKConstant.sol";
-import {HelperScript} from "../../script/Helper/HelperScript.s.sol";
+import {DeploySRSTK} from "../../script/DeploySRSTK.s.sol";
+import {RSTKToken} from "../../src/RSTK/token/RSTKToken.sol";
+import {PriceOracle} from "../../src/Oracle/PriceOracle.sol";
+import {PortfolioBalance} from "../../src/automation/PortfolioBalance.sol";
 import {IContractStruct} from "../../src/RSTK/IContractStruct.sol";
+import {HelperScript} from "../../script/Helper/HelperScript.s.sol";
+import {TokenScript} from "../../script/Helper/TokenScript.s.sol";
+import {FunctionsSubscriptions} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/FunctionsSubscriptions.sol";
+import {MockLinkToken} from "@chainlink/contracts/src/v0.8/mocks/MockLinkToken.sol";
+import {IRWAAccessControl} from "../../src/access/IRWAAccessControl.sol";
+import {MockERC20} from "@chainlink/contracts/src/v0.8/vendor/forge-std/src/mocks/MockERC20.sol";
+
 contract SRSTKTest is Test {
     SRSTK public s_srstk;
+    DeploySRSTK public s_deploySRSTK;
+    RSTKToken public s_rstkToken;
+    PriceOracle public s_priceOracle;
+    PortfolioBalance public s_portfolioBalance;
+    HelperScript public s_helperScript;
     TokenScript public s_tokenScript;
-    HelperScript public s_scriptPriceOracle;
-    // string public constant SOURCE = "./functions/source/sourcePortfolio.js";
+    IContractStruct.RequestConfig public s_requestConfig;
+    IContractStruct.RequestData public s_requestData;
+    IContractStruct.ExtraInfo public s_extraInfo;
+    string public constant SOURCE = "./functions/source/sourcePortfolio.js";
+    address public USER = makeAddr("user");
     function setUp() public {
+        s_helperScript = new HelperScript(SOURCE);
         s_tokenScript = new TokenScript();
-        s_scriptPriceOracle = new HelperScript("");
-        (IContractStruct.RequestData memory config  , IContractStruct.RequestConfig memory config2)= s_scriptPriceOracle.getNetworkConfig();
-        IContractStruct.ExtraInfo memory aggregator = s_tokenScript.getNetworkConfig();
-        s_srstk = new SRSTK(
-            config,
-            aggregator
-        );
+        ( s_requestData  , s_requestConfig) = s_helperScript.getNetworkConfig();
+        s_extraInfo = s_tokenScript.getNetworkConfig();
+        s_priceOracle = new PriceOracle(s_extraInfo.owner,s_requestData,s_requestConfig);
+        s_portfolioBalance = new PortfolioBalance(s_extraInfo.owner,s_requestData,s_requestConfig);
+        s_rstkToken = new RSTKToken(s_requestData.accessControlAddress);
+        s_deploySRSTK = new DeploySRSTK();  
+        s_deploySRSTK.run(SOURCE,s_extraInfo.srstkTokenAddr,s_extraInfo.priceOracleAddr,s_extraInfo.portfolioContractAddr);
+        vm.startPrank(address(s_tokenScript));
+        IERC20(s_extraInfo.usdc)._mint(USER , 100e6);
+        vm.stopPrank();
+    }
+
+    function testSendRequest() public {
+        
+
+
     }
 
 }
