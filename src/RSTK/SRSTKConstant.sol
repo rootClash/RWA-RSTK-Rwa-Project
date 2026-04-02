ccee5fece5b80cfd83cdbec0e80d1321393fd1ef
hackflu <satyamkumar45381@gmail.com>
2026-04-02 10:10:53 +0000

    add: add new constant

--- src/RSTK/SRSTKConstant.sol
+++ src/RSTK/SRSTKConstant.sol
@@ -5,8 +5,9 @@ library SRSTKConstant {
     uint256 public constant PRECISION = 1e18;
     uint256 public constant MIN_COLLATERAL_BPS = 15_000;
     uint256 public constant BPS_DENOMINATOR = 10_000;
-    string public constant SRC_PORTFOLIO = "";
-    string public constant SRC_MINT = "";
-    string public constant SRC_BURN = "";
+    string public constant SRC_BUY = "./functions/source/sourceBuy.js";
+    string public constant SRC_SELL = "./functions/source/sourceSell.js";
+    string public constant SRC_CHECKPRICE = "./functions/source/source.js";
+    string public constant SRC_CHECKBALANCEAVL = "./functions/source/sourcePortfolio.js";
     uint256 public constant MINIMUM_DEPSOIT = 10;
 }
