3bc2e31d121386f5807e8fffa355a2eeac9232f5
hackflu <satyamkumar45381@gmail.com>
2026-04-05 07:58:34 +0000

    add : add the mint token functions

--- src/RSTK/SRSTK.sol
+++ src/RSTK/SRSTK.sol
@@ -19,31 +19,37 @@ import {IPriceOracle} from "../Oracle/IPriceOracle.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SRSTKConstant} from "./SRSTKConstant.sol";
 import {IPortfolioBalance} from "../automation/IPortfolioBalance.sol";
+import {Allowlist} from "../allowlist/Allowlist.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+import {
+    AutomationCompatibleInterface
+} from "@chainlink/contracts/src/v0.8/automation/interfaces/AutomationCompatibleInterface.sol";
+import {IMarketPhase} from "../MarketCheck/IMarketPhase.sol";
 
 contract SRSTK is FunctionsClient, ConfirmedOwner, IContractStruct {
     /*//////////////////////////////////////////////////////////////
                             TYPE DECLERATION
     //////////////////////////////////////////////////////////////*/
     using FunctionsRequest for FunctionsRequest.Request;
+    using Strings for uint256;
     /*//////////////////////////////////////////////////////////////
                              STATE VARIABLE
     //////////////////////////////////////////////////////////////*/
     uint256 private s_portfolioBalance;
-    bytes32 private s_depositRequestId;
-    bytes32 private s_burnRequestId;
-    uint256 private s_priceIdCounter = 1;
     uint256 private s_totalContractBalance;
-    uint256 private s_totalUserBalance;
-    uint256[] private s_totalDepositors;
-    uint256[] private s_totalBurners;
-    mapping(address => uint256[]) private s_depositorData;
-    mapping(uint256 => IContractStruct.Depositor) private s_depositStruct;
-    mapping(address => uint256[]) private s_redeemerData;
-    mapping(uint256 => IContractStruct.Redeemer) private s_redmerStruct;
-    mapping(bytes32 => IContractStruct.RequestType) private s_requestType;
-    mapping(IContractStruct.RequestType => IContractStruct.RequestConfig)
-        private s_request;
-
+    uint256 private s_depositedAmountUsedForBuy;
+    uint256 private s_currentMintBatchId;
+    mapping(bytes32 => uint256) private s_requestIdToBatchId;
+    mapping(uint256 => bool) private s_batchMintFulfilled;
+    bytes32 private s_latestRequestId;
+    mapping(address => mapping(bytes32 => IContractStruct.Depositor))
+        private s_depositorData;
+    mapping(address => bytes32[]) private s_depositorIds;
+    mapping(address => mapping(bytes32 => IContractStruct.Redeemer))
+        private s_redeemerData;
+    mapping(address => bytes32[]) private s_redmerIds;
+    mapping(bytes32 => IContractStruct.RequestType) private s_requestIdType;
+    mapping(bytes32 => IContractStruct.RequestPhase) private s_requestPhase;
     uint64 immutable i_subscriptionId;
     bytes32 immutable i_donId;
     uint32 immutable i_gasLimit;
@@ -54,29 +60,41 @@ contract SRSTK is FunctionsClient, ConfirmedOwner, IContractStruct {
     address immutable i_srstkTokenAddr;
     address immutable i_priceOracleAddr;
     address immutable i_portfolioBalance;
+    address immutable i_marketPhaseContractAddr;
     AggregatorV3Interface internal immutable i_aggregator;
     /*//////////////////////////////////////////////////////////////
                                 EVENT       
     //////////////////////////////////////////////////////////////*/
     event DepositRequested(
-        bytes32 indexed requestId,
         address indexed depositor,
-        uint256 amount,
-        uint256 priceId
+        bytes32 indexed requestId,
+        uint256 amountOfTokenToMint,
+        uint256 minimumCollateralRequired
+    );
+    event BurnRequested(
+        address indexed redeemer,
+        bytes32 indexed requestId,
+        uint256 amountOfTokenToBurn,
+        uint256 minimumCollateralExpected
     );
     event PortfolioBalance(uint256 newBalance);
-
+    event ClientIdReceived(string clientId, uint256 batchId);
     /*//////////////////////////////////////////////////////////////
                                  ERRORS
     //////////////////////////////////////////////////////////////*/
     error SRSTK__AmountCannotBeZero();
-    error SRSTK__InvalidRequestType();
-    error SRSTK__InsufficientCollateral();
-    error SRSTK__InvalidPriceId();
-    error SRSTK__RequestAlreadyFulfilled();
+    error SRSTK__InsufficientBalance();
     error SRSTK__TransactionNotSuccesed();
     error SRSTK__InsufficientReserve();
-    error SRSTK__InvalidIndex();
+    error SRSTK__InvalidRequestId();
+    error SRSTK__InvalidRequestType();
+    error SRSTK__RequestCannotBeProcessed();
+    error SRSTK__MakeMintRequestAgain();
+    error SRSTK__MarketIsClosed();
+    error SRSTK__TokenTranferToContractFailed();
+    error SRSTK__UpkeepNotNeeded();
+    error SRSTK__InvalidClientId();
+
     /*//////////////////////////////////////////////////////////////
                                 MODIFIERS
     //////////////////////////////////////////////////////////////*/
@@ -110,71 +128,132 @@ contract SRSTK is FunctionsClient, ConfirmedOwner, IContractStruct {
                                 EXTERNAL
     //////////////////////////////////////////////////////////////*/
     function sendMintRequest(
-        uint256 amount
-    ) external requireToCheckAmount(amount) returns (bytes32) {
-        uint256 minimumCollateral = minimumCollateralNeeded(amount);
-        if (minimumCollateral > IERC20(i_usdcAddress).balanceOf(msg.sender)) {
-            revert SRSTK__InsufficientCollateral();
-        }
-        IContractStruct.RequestConfig memory config = setRequestConfig(
-            SRSTKConstant.SRC_MINT,
-            new string[](0),
-            new bytes[](0)
+        uint256 amountOfTokenToMint
+    ) external requireToCheckAmount(amountOfTokenToMint) returns (bytes32) {
+        uint256 minimumCollateral = minimumCollateralNeeded(
+            amountOfTokenToMint
         );
-        bytes32 requestId = makeRequestToDon(config);
-        s_depositRequestId = requestId;
-        uint256 priceId = s_priceIdCounter++;
-        s_totalContractBalance += amount;
+        if (minimumCollateral > IERC20(i_usdcAddress).balanceOf(msg.sender)) {
+            revert SRSTK__InsufficientBalance();
+        }
+        uint256 currentCashAvailable = getPortfolioBalance();
+        if (currentCashAvailable <= minimumCollateral) {
+            revert SRSTK__InsufficientReserve();
+        }
+        bytes32 priceId = keccak256(
+            abi.encodePacked(msg.sender, amountOfTokenToMint, block.timestamp)
+        );
         IContractStruct.Depositor memory depositor = IContractStruct.Depositor({
             depositorAddress: msg.sender,
-            amountDeposited: amount,
+            amountToMint: amountOfTokenToMint,
             priceId: priceId,
-            fullfilled: false,
-            collateralPaid: minimumCollateral, ////// check krna esko
-            requestType: IContractStruct.RequestType.MINT
+            batchId: s_currentMintBatchId,
+            redeemed: false,
+            collateralPaid: minimumCollateral,
+            notionalAmount: 0
         });
-        s_depositorData[msg.sender].push(priceId);
-        s_depositStruct[priceId] = depositor;
-        s_requestType[requestId] = IContractStruct.RequestType.MINT;
-        s_request[IContractStruct.RequestType.MINT] = config;
-
+        s_depositorData[msg.sender][priceId] = depositor;
+        s_depositorIds[msg.sender].push(priceId);
+        s_totalContractBalance += minimumCollateral; /// this is the total contract balance
+        uint256 notionalAmount = getLimitExposer(depositor.collateralPaid);
+        depositor.notionalAmount = notionalAmount;
+        s_depositedAmountUsedForBuy += notionalAmount; /// this is the total amount for buying the stocks.
         bool success = IERC20(i_usdcAddress).transferFrom(
             msg.sender,
             address(this),
-            amount
+            minimumCollateral
         );
         if (!success) {
             revert SRSTK__TransactionNotSuccesed();
         }
-        emit DepositRequested(requestId, msg.sender, amount, priceId);
-        return requestId;
+        emit DepositRequested(
+            msg.sender,
+            priceId,
+            amountOfTokenToMint,
+            minimumCollateral
+        );
+        return priceId;
     }
 
-    function sendBurnRequest() external {}
+    function sendBurnRequest(
+        uint256 amountOfTokenToBurn
+    ) external requireToCheckAmount(amountOfTokenToBurn) returns (bytes32) {
+        if (
+            IERC20(i_srstkTokenAddr).balanceOf(msg.sender) < amountOfTokenToBurn
+        ) {
+            revert SRSTK__InsufficientBalance();
+        }
 
-    function Redeem(uint256 priceId) external {
-        // CEI
-        if (priceId >= s_depositorData[msg.sender].length) {
-            revert SRSTK__InvalidIndex();
-        }
-        uint256 actualId = s_depositorData[msg.sender][priceId];
-        IContractStruct.Depositor storage depositor = s_depositStruct[actualId];
-        if (depositor.priceId == 0) {
-            revert SRSTK__InvalidPriceId();
-        }
-        if (depositor.fullfilled) {
-            revert SRSTK__RequestAlreadyFulfilled();
-        }
-        if (depositor.requestType != IContractStruct.RequestType.MINT) {
-            revert SRSTK__InvalidRequestType();
-        }
-        //// check the portFolio balance
-        uint256 portFolioBalance = getPortfolioBalance();
-        if (portFolioBalance < depositor.amountDeposited) {
+        uint256 minCollateralExpected = minimumCollateralNeeded(
+            amountOfTokenToBurn
+        );
+        if (
+            minCollateralExpected > IERC20(i_usdcAddress).balanceOf(msg.sender)
+        ) {
             revert SRSTK__InsufficientReserve();
         }
-        //// check whether the stocks buyed?
-        
+        bytes32 priceId = keccak256(
+            abi.encodePacked(msg.sender, amountOfTokenToBurn, block.timestamp)
+        );
+
+        IContractStruct.Redeemer memory redeemer = IContractStruct.Redeemer({
+            redeemed: false,
+            priceId: priceId,
+            user: msg.sender,
+            amountToTokenBurned: amountOfTokenToBurn,
+            minCollateralExpected: minCollateralExpected
+        });
+
+        s_redeemerData[msg.sender][priceId] = redeemer;
+        s_redmerIds[msg.sender].push(priceId);
+
+        bool success = IERC20(i_srstkTokenAddr).transferFrom(
+            msg.sender,
+            address(this),
+            amountOfTokenToBurn
+        );
+        if (!success) {
+            revert SRSTK__TransactionNotSuccesed();
+        }
+
+        emit BurnRequested(
+            msg.sender,
+            priceId,
+            amountOfTokenToBurn,
+            minCollateralExpected
+        );
+
+        return priceId;
+    }
+
+    function redeemUsdc() external {}
+
+    function redeemTokens(bytes32 priceId, uint256 requestIdIndex) external {
+        /// CEI
+        if (s_depositorIds[msg.sender][requestIdIndex] != priceId) {
+            revert SRSTK__InvalidRequestId();
+        }
+        IContractStruct.Depositor storage depositor = s_depositorData[
+            msg.sender
+        ][priceId];
+        if (depositor.redeemed) {
+            revert SRSTK__MakeMintRequestAgain();
+        }
+        if (!s_batchMintFulfilled[depositor.batchId]) {
+            revert SRSTK__RequestCannotBeProcessed();
+        }
+
+        depositor.redeemed = true;
+        depositor.collateralPaid =
+            depositor.collateralPaid -
+            depositor.notionalAmount;
+        bool success = IERC20(i_srstkTokenAddr).transfer(
+            msg.sender,
+            depositor.amountToMint
+        );
+        if (!success) {
+            revert SRSTK__TransactionNotSuccesed();
+        }
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -184,16 +263,20 @@ contract SRSTK is FunctionsClient, ConfirmedOwner, IContractStruct {
         return _getUSDCPriceInUSD();
     }
 
-    function getCurrentRequestIntent(
+    //// use the modifier for checking the user in allowlist
+    function getRequestIntent(
         bytes32 requestID
     ) external view returns (IContractStruct.RequestType) {
-        return s_requestType[requestID];
+        return s_requestIdType[requestID];
     }
 
-    function getUserRequestID(
-        address user
-    ) external view returns (uint256[] memory depositorData) {
-        depositorData = s_depositorData[user];
+    /// add the modifier to check that the address has been added to the Allowlist
+    function getUserRequestIDs()
+        external
+        view
+        returns (bytes32[] memory depositorData)
+    {
+        depositorData = s_depositorIds[msg.sender];
     }
 
     function getPortfolioBalance() public returns (uint256) {
@@ -205,22 +288,72 @@ contract SRSTK is FunctionsClient, ConfirmedOwner, IContractStruct {
     }
 
     /*//////////////////////////////////////////////////////////////
+                                 PUBLIC
+    //////////////////////////////////////////////////////////////*/
+    function minimumCollateralNeeded(
+        uint256 numberOfShares
+    ) public view returns (uint256 minCollateralRequired) {
+        uint256 assetRequired = _calculateRequiredValue(numberOfShares);
+        minCollateralRequired =
+            (assetRequired * SRSTKConstant.MIN_COLLATERAL_BPS) /
+            SRSTKConstant.BPS_DENOMINATOR;
+    }
+
+    function getLimitExposer(
+        uint256 collateral
+    ) public pure returns (uint256 limitExposer) {
+        limitExposer =
+            (collateral * SRSTKConstant.BPS_DENOMINATOR) /
+            SRSTKConstant.MIN_COLLATERAL_BPS;
+    }
+
+    /*//////////////////////////////////////////////////////////////
                                 INTERNAL
     //////////////////////////////////////////////////////////////*/
+    function buyStocks() internal returns (bytes32) {
+        string[] memory argsValue = new string[](1);
+        argsValue[0] = s_depositedAmountUsedForBuy.toString();
+        bytes32 requestId = _makeRequestToDon(
+            IContractStruct.RequestConfig({
+                source: SRSTKConstant.SRC_BUY,
+                args: argsValue,
+                bytesArgs: new bytes[](0)
+            })
+        );
+        s_requestPhase[requestId] = IContractStruct.RequestPhase.START;
+        return requestId;
+    }
+
+    functions sellStocks() internal returns (bytes32) {
+        string[] memory argsValue = new string[](1);
+        argsValue[0] = s_depositedAmountUsedForBuy.toString();
+        bytes32 requestId = _makeRequestToDon(
+            IContractStruct.RequestConfig({
+                source: SRSTKConstant.SRC_SELL,
+                args: argsValue,
+                bytesArgs: new bytes[](0)
+            })
+        );
+        s_requestPhase[requestId] = IContractStruct.RequestPhase.START;
+        return requestId;
+    }
+
     function fulfillRequest(
         bytes32 requestId,
         bytes memory response,
         bytes memory err
     ) internal override {
-        if (s_requestType[requestId] == IContractStruct.RequestType.MINT) {
-            _handleMintToken();
+        if (s_requestIdType[requestId] == IContractStruct.RequestType.MINT) {
+            _handleMintToken(response, requestId);
         } else if (
-            s_requestType[requestId] == IContractStruct.RequestType.BURN
+            s_requestIdType[requestId] == IContractStruct.RequestType.BURN
         ) {
-            _handleBurnToken();
+            // _handleBurnToken(response);
         } else {
             revert SRSTK__InvalidRequestType();
         }
+        s_requestIdType[requestId] = IContractStruct.RequestType.NONE;
+        s_requestPhase[requestId] = IContractStruct.RequestPhase.DONE;
     }
 
     function _getUSDCPriceInUSD() internal view returns (uint256) {
@@ -228,23 +361,14 @@ contract SRSTK is FunctionsClient, ConfirmedOwner, IContractStruct {
         return uint256(price);
     }
 
-    function minimumCollateralNeeded(
-        uint256 anountToMint
-    ) public view returns (uint256 minCollateralRequired) {
-        uint256 assetRequired = _calculateRequiredValue(anountToMint);
-        minCollateralRequired =
-            (assetRequired * SRSTKConstant.MIN_COLLATERAL_BPS) /
-            SRSTKConstant.BPS_DENOMINATOR;
-    }
-
     function _calculateRequiredValue(
-        uint256 amountOfTokenToMint
+        uint256 numberOfTokens
     ) internal view returns (uint256 assetRequired) {
         uint256 priceOfRsst = IPriceOracle(i_priceOracleAddr).getPrice();
-        assetRequired = priceOfRsst * amountOfTokenToMint;
+        assetRequired = priceOfRsst * numberOfTokens;
     }
 
-    function makeRequestToDon(
+    function _makeRequestToDon(
         IContractStruct.RequestConfig memory config
     ) internal returns (bytes32) {
         FunctionsRequest.Request memory req;
@@ -264,21 +388,54 @@ contract SRSTK is FunctionsClient, ConfirmedOwner, IContractStruct {
         return requestID;
     }
 
-    function setRequestConfig(
-        string memory source,
-        string[] memory args,
-        bytes[] memory bytesArgs
-    ) internal returns (IContractStruct.RequestConfig memory config) {
-        config = IContractStruct.RequestConfig({
-            source: source,
-            args: args,
-            bytesArgs: bytesArgs
-        });
-    }
+    function _handleMintToken(
+        bytes memory response,
+        bytes32 requestId
+    ) internal {
+        string memory clientId = abi.decode(response, (string));
+        if (bytes(clientId).length == 0) {
+            revert SRSTK__InvalidClientId();
+        }
 
-    function _handleMintToken() internal {}
+        uint256 batchId = s_requestIdToBatchId[requestId];
+        s_batchMintFulfilled[batchId] = true;
+
+        emit ClientIdReceived(clientId, batchId);
+    }
 
     function _handleBurnToken() internal {}
 
-    function portFolio() internal {}
+    function checkUpkeep(
+        bytes calldata /*checkData*/
+    ) internal returns (bool upkeepNeeded, bytes memory /*performData*/) {
+        (bool state, uint256 openingTime, uint256 closeTime) = IMarketPhase(
+            i_marketPhaseContractAddr
+        ).getMarketPhase();
+        bool checkTime = openingTime > block.timestamp &&
+            closeTime < block.timestamp &&
+            state == true;
+            
+        upkeepNeeded = checkTime && (s_depositedAmountUsedForBuy > 0);
+        return (upkeepNeeded, "");
+    }
+
+    function performUpkeep(bytes calldata performData) external {
+        (bool upkeepNeeded, ) = checkUpkeep(performData);
+        if (!upkeepNeeded) {
+            revert SRSTK__UpkeepNotNeeded();
+        }
+        // Double check to ensure we don't send empty requests
+        if (s_depositedAmountUsedForBuy == 0) {
+            revert SRSTK__AmountCannotBeZero();
+        }
+        
+        bytes32 requestId = buyStocks();
+        s_latestRequestId = requestId;
+
+        s_requestIdToBatchId[requestId] = s_currentMintBatchId;
+        s_currentMintBatchId++; // Advance to the next batch for future depositors
+        s_depositedAmountUsedForBuy = 0; // Reset the aggregator so we don't double-buy next upkeep!
+        s_requestIdType[requestId] = IContractStruct.RequestType.MINT;
+        s_requestPhase[requestId] = IContractStruct.RequestPhase.PENDING;
+    }
 }
